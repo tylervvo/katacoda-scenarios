3fc5c379cfc979a81ac971d249b889edd981bbda
Steve Calnan <steve.calnan@datadoghq.com>
2020-07-29 12:54:33 -0700

    removed log generation on startup; not possible

--- dd-201/background.sh
+++ dd-201/background.sh
@@ -17,6 +17,3 @@ sed -i "/volumes:/a \ \ \ \ \ \ - /sys/kernel/debug:/sys/kernel/debug" /ecommwor
 
 # Get the latest lab tools
 curl -s https://datadoghq.dev/katacodalabtools/r?raw=true|bash
-
-# Generate some logs for cost/price
-python /root/lab/postlogs.py 100 
