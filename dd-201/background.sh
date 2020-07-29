4a5a5d1eae26a44d813f250b3a83f6b367e35f21
Steve Calnan <steve.calnan@datadoghq.com>
2020-07-29 13:00:33 -0700

    fixed background docker env

--- dd-201/background.sh
+++ dd-201/background.sh
@@ -2,7 +2,7 @@
 ln -s /ecommworkshop /root/lab
 
 # Enable system probe for NPM
-sed -i "/- DD_TAGS='env:dd201'/r"<(
+sed -i "/- DD_TAGS='env:ruby-shop'/r"<(
     echo "      - DD_SYSTEM_PROBE_ENABLED=true"
     echo "    cap_add:"
     echo "      - SYS_ADMIN"
@@ -14,6 +14,10 @@ sed -i "/- DD_TAGS='env:dd201'/r"<(
     echo "      - apparmor:unconfined"
 ) /ecommworkshop/docker-compose-files/docker-compose-fixed-instrumented.yml
 sed -i "/volumes:/a \ \ \ \ \ \ - /sys/kernel/debug:/sys/kernel/debug" /ecommworkshop/docker-compose-files/docker-compose-fixed-instrumented.yml
+
+# workshop-specific environment
+sed -i "/env:ruby-shop/env:dd201/" /ecommworkshop/docker-compose-files/docker-compose-fixed-instrumented.yml
+
 
 # Get the latest lab tools
 curl -s https://datadoghq.dev/katacodalabtools/r?raw=true|bash
