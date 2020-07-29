90b53727a6212aabad1d5171bf893d7fbac6dcf2
Steve Calnan <steve.calnan@datadoghq.com>
2020-07-29 01:30:53 -0700

    Changed source attribute in postlogs script to python

--- dd-201/assets/postlogs.py
+++ dd-201/assets/postlogs.py
@@ -29,7 +29,7 @@ headers = {
 url = 'https://http-intake.logs.datadoghq.com/v1/input'
 payload = {
   "ddsource": "python",
-  "ddtags": "env:ruby-shop, service:store-frontend, source:ruby, team:frontend",
+  "ddtags": "env:ruby-shop, service:store-frontend, source:python, team:frontend",
   "hostname": "host01",
   "message": "{'event':'add_to_cart','item':" + json.dumps(random.choice(items)) + "}",
   "service": "store-frontend"
